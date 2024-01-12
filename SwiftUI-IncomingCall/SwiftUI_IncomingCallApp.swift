6e8aeede3c2321b47bba6be4def6dc649af844dd
SreytouchLang <atouch.it@gmail.com>
2024-01-12 14:51:25 -0800

    Initial Commit

---
+++ SwiftUI-IncomingCall/SwiftUI_IncomingCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IncomingCallApp.swift
+//  SwiftUI-IncomingCall
+//
+//  Created by Sreytouch(Jessica) on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IncomingCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
